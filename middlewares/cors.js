1b13af0cc5bdb71bf5bfe3d909eae3fa64a2d26c
Mateo Cao <mateocao12@gmail.com>
2023-11-28 15:42:46 -0300

    fix: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,9 @@
 import cors from 'cors';
 
 const ACCEPTED_ORIGINS = [
-  'http://127.0.0.1:5500',
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://main--effortless-custard-7e0ce8.netlify.app'
+  'https://www.mispelis.com.ar/'
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
